637f3b0d3846e18a4dc58e82c1e9069e2a236bd4
Harsha <95213910+Devilharsha@users.noreply.github.com>
2022-02-23 21:29:42 +0530

    Update start_text.py

--- bot/plugins/start_text.py
+++ bot/plugins/start_text.py
@@ -27,7 +27,7 @@ START_TEXT = f"""
 """
 
 button = InlineKeyboard(row_width=1)
-button.add(InlineKeyboardButton(text="SENKUBOTS", url="t.me/SENKUBOTS"), InlineKeyboardButton(text="Owner", url=f"t.me/harshahero "))
+button.add(InlineKeyboardButton(text="SENKUBOTS", url="t.me/SENKUBOTS"), InlineKeyboardButton(text="Owner", url=f"t.me/harshahero"))
 
 @Bot.on_message(
     filters.command(START_COMMAND, COMMM_AND_PRE_FIX) &
